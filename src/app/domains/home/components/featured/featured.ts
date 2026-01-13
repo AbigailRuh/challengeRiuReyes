ac91669264adac1b103502a1f5270dc8c7f98ca1
Abigail Reyes Ruh <abi3010ruh@gmail.com>
2026-01-13 12:55:25 -0300

    applying changes commented after PR

--- src/app/domains/home/components/featured/featured.ts
+++ src/app/domains/home/components/featured/featured.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-featured',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './featured.html',
   styleUrl: './featured.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Featured {
-
-}
+export class Featured {}
